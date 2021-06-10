76ef52613c57c7affd6badc559063fa670f9c577
maxime acciaioli <AcciaioliMaxime@gmail.com>
2021-06-10 16:30:26 +0200

    Index filtré v.1

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,18 +4,43 @@ class PetsController < ApplicationController
 
   def index
     @pets = policy_scope(Pet)
-    if params[:Type].present? && params[:City].present? && params[:Range].present?
-      @city = params[:City]
-      @category = params[:Type]
-      @kms = params[:Range]
-      @pets = @pets.near(@city, @kms).select{ |pet| pet.category == @category }
-    elsif params[:Type].present?
-      @pets = Pet.where(category: params[:Type])
-    elsif params[:City].present?
-      @city = params[:City]
-      @kms = params[:Range]
-      @pets = Pet.near(@city, @kms)
+    
+    @city = params[:City] if params[:City].present?
+    @kms = params[:Range] if params[:Range].present?
+    
+    params[:Type].present? ? @category = params[:Type] : @category = nil
+    params[:Size].present? ? @size = params[:Size] : @size = nil
+    params[:Hair].present? ? @hair = params[:Hair] : @hair = nil
+    params[:Personality].present? ? @personality = params[:Personality] : @personality = nil
+    params[:Gender].present? ? @gender = params[:Gender] : @gender = nil
+    params[:Age].present? ? @age = params[:Age] : @age = nil
+
+
+    if !@city.nil? && !@kms.nil?
+      @pets = @pets.near(@city, @kms)
     end
+
+      @pets = @pets.where(category: @category) unless @category.nil?
+      @pets = @pets.where(size: @size) unless @size.nil?
+      @pets = @pets.where(hair: @hair) unless @hair.nil?
+      @pets = @pets.where(personality: @personality) unless @personality.nil?
+      @pets = @pets.where(gender: @gender) unless @gender.nil?
+      @pets = @pets.where(age: @age) unless @age.nil?
+   
+
+    #if params[:Type].present? && params[:City].present? && params[:Range].present?
+     # @city = params[:City]
+      #@category = params[:Type]
+      #@kms = params[:Range]
+      #@pets = @pets.near(@city, @kms).select{ |pet| pet.category == @category }
+    #elsif params[:Type].present?
+     # @pets = Pet.where(category: params[:Type])
+    #elsif params[:City].present?
+     # @city = params[:City]
+      #@kms = params[:Range]
+      #@pets = Pet.near(@city, @kms)
+    #end
+
     # @markers = @pets.geocoded.map do |pet|
     #   {
     #     lat: pet.latitude,
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,16 +4,21 @@ class Pet < ApplicationRecord
   has_many_attached :photos
 
   TYPES = ["Chien", "Chat"]
+  SIZE = ["Petit", "Moyen", "Grand"]
+  HAIR = ["Court", "Long"]
+  PERSONALITY = ["Sociable", "Joueur", "Calme", "Energique", "Fort caractère"]
+  GENDER = ["Mâle", "Femelle"]
+  
   validates :category, presence: true, inclusion: { in: TYPES }
   validates :name, presence: true
   validates :age, presence: true, numericality: { only_integer: true }
   validates :description, presence: true, length: { minimum: 50 }
   validates :address, presence: true
   validates :race, presence: true
-  validates :size, presence: true, inclusion: { in: ["Petit", "Moyen", "Grand"] }
-  validates :hair, presence: true, inclusion: { in: ["Court", "Long"] }
-  validates :personality, presence: true, inclusion: { in: ["Sociable", "Joueur", "Calme", "Energique", "Fort caractère"] }
-  validates :gender, presence: true, inclusion: { in: ["Mâle", "Femelle"] }
+  validates :size, presence: true, inclusion: { in: SIZE }
+  validates :hair, presence: true, inclusion: { in: HAIR }
+  validates :personality, presence: true, inclusion: { in: PERSONALITY }
+  validates :gender, presence: true, inclusion: { in: GENDER }
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?

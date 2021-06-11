9bbc51a40b01baf1912d658fd907246f29fb71c8
lisagentili <lisa.gentili@kedgebs.com>
2021-06-11 16:49:20 +0200

    profil pix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :age, :address, :description, :habitation_type, :garden, :animals, :children])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :age, :address, :description, :habitation_type, :garden, :animals, :children, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :age, :address, :description, :habitation_type, :garden, :animals, :children])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :age, :address, :description, :habitation_type, :garden, :animals, :children, :photo])
   end
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
